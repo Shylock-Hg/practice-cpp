93c809d1cebe436d5e23a0b2688e35e9ed82f18a
Shylock Hg <tcath2s@gmail.com>
2018-09-14 18:57:13 +0800

    Add reference practice

--- for-range.cc
+++ for-range.cc
@@ -3,9 +3,13 @@
 int main(int argc, char * argv[]){
 
 	int array[] {-1,2,4,99};
-
 	for(auto i : array){
 		std::cout << i << std::endl;
+	}
+
+	char * string = "hello";
+	for(auto c : string){
+		
 	}
 
 	return 0;
