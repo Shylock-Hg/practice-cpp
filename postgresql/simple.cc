ccefca190e646128ea7923c14644acb303c9409c
Shylock Hg <tcath2s@gmail.com>
2018-10-23 16:17:23 +0800

    Simple postgresql usage app with created db name `db-shylock` and user name `shylock`

--- postgresql/simple.cc
+++ postgresql/simple.cc
@@ -1,32 +1,61 @@
 #include <iostream>
+#include <iterator>
 #include <pqxx/pqxx>
 
 int main()
 {
-    try
-    {
+    try {
         pqxx::connection C("dbname=db-shylock");
         std::clog << "Connected to " << C.dbname() << std::endl;
         pqxx::work W(C);
 
-        pqxx::result R = W.exec("SELECT name FROM cities");
-
-        std::clog << "Found " << R.size() << " cities:" << std::endl;
-        for (auto row: R)
-            std::clog << row[0].c_str() << std::endl;
-
-        //std::clog << "Doubling all employees' salaries..." << std::endl;
-        //W.exec("UPDATE employee SET salary = salary*2");
+	//< create table cities and weathers
+	W.exec("CREATE TABLE cities ("
+	       "    name      varchar(80),"
+	       "    location  point"
+	       ");");
+	W.exec("CREATE TABLE weathers ("
+               "city     varchar(80),"
+               "temp_lo  int,        "//"   -- low temperature"
+               "temp_hi  int,        "//"   -- high temperature"
+               "prcp     real,       "//"   -- precipitation"
+               "date     date"
+	       ");");
 
         std::clog << "Insert new raw ..." << std::endl;
-        W.exec("INSERT INTO cities VALUES ('shanghai', '(1.1, 2.2)')");
+        W.exec("INSERT INTO cities VALUES ('shanghai', '(1.1, 2.2)'), "
+			"('sichuan', '(1.1, 2.2)');");
+	
+	W.exec("INSERT INTO weathers VALUES "
+			"('shanghai', '22', '33', '3.3', '(1992, 2, 13)'), "
+			"('sichuan', '22', '33', '3.3', '(1992, 2, 13)');");
+	W.commit();
 
-        std::clog << "Making changes definite: ";
-        W.commit();
-        std::clog << "OK." << std::endl;
+
+	pqxx::nontransaction N(C);
+	pqxx::result R = N.exec("SELECT name, date FROM cities, weathers;");
+	std::clog << "Found " << R.size() << " Items:" << std::endl;
+	for (const auto& row : R) {
+		//std::clog << row[0] << "  " << row[1] << std::endl;
+		for (const auto& item : row) {
+			std::clog << item << "  ";
+		}
+		std::clog << std::endl;
+	}
+
+	pqxx::result R2 = N.exec("SELECT * FROM cities;");
+	std::clog << "Found " << R2.size() << " Items:" << std::endl;
+	for (const auto& row : R2) {
+		//std::clog << row[0] << "  " << row[1] << std::endl;
+		for (const auto& item : row) {
+			std::clog << item << "  ";
+		}
+		std::clog << std::endl;
+	}
+
+	C.disconnect();
     }
-    catch (const std::exception &e)
-    {
+    catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
         return 1;
     }
