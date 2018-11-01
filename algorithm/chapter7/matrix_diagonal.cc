1b72a3654aee6f7c330dd0c7274e880a2e41c743
Shylock-Hg <tcath2s@gmail.com>
2018-11-01 09:27:02 +0000

    Implement a diagonal matrix by array

--- algorithm/chapter7/matrix_diagonal.cc
+++ algorithm/chapter7/matrix_diagonal.cc
@@ -4,3 +4,34 @@
  *  \email tcath2s@gmail.com
  * */
 
+#include "matrix_diagonal.hh"
+
+int main (int argc, char * argv[]) {
+        matrix_diagonal<int, 3, 2> a {1, 2};
+        // matrix_diagonal<int, 2, 3> b {1, 2, 3, 4};  //!< throw exception
+        matrix_diagonal<int, 2, 3> b {1, 2};
+        std::cout << a << std::endl;
+        std::cout << a*b << std::endl;
+
+        matrix_diagonal<float, 3, 2> c {1, 2};
+        matrix_diagonal<float, 2, 5> d {1, 2};
+        std::cout << c*d << std::endl;
+        std::cout << c(1, 1) << "  " << c(0, 1) << std::endl;
+        std::cout << c.transpose() << std::endl;
+        // std::cout << c(2, 2) << std::endl;  //!< throw exception
+
+        matrix_diagonal<int, 1, 2> e {1};
+        matrix_diagonal<int, 2, 1> f {3};
+        std::cout << e*f << std::endl;
+        std::cout << -e << (+(-e)) << std::endl;
+
+        matrix_diagonal<int, 1, 2> g {1};
+        matrix_diagonal<int, 1, 2> h {2};
+        std::cout << (g-h) << (g+h) << std::endl;
+        std::cout << g.transpose() << std::endl;
+
+        matrix_diagonal<int, 1, 2> i {-1};
+        std::cout << (+i) << std::endl;
+        std::cout << i.row() << "  " << i.col() << std::endl;
+        return 0;
+}
--- algorithm/chapter7/matrix_diagonal.hh
+++ algorithm/chapter7/matrix_diagonal.hh
@@ -7,7 +7,163 @@
 #ifndef _MATRIX_DIAGONAL_HH_
 #define _MATRIX_DIAGONAL_HH_
 
+#include <initializer_list>
+#include <iostream>
+#include <iterator>
+#include <stdexcept>
+#include <memory>
+#include <algorithm>
 
+template <class __item__, std::size_t ROW, std::size_t COL>
+class matrix_diagonal {
+private:
+public:
+        static constexpr std::size_t diagonal = std::min(ROW, COL);
+
+        __item__* ele;  //!< just diagnal elements
+
+        matrix_diagonal() : ele {new __item__[diagonal]} {};
+
+        matrix_diagonal(std::initializer_list<__item__> list) :
+                ele {new __item__[diagonal]} {
+                if (std::min(ROW, COL) < list.size()) {
+                        throw std::out_of_range("Initializer list out of range!\n");
+                }
+                std::copy(list.begin(), list.end(), ele);
+        };
+
+        matrix_diagonal(matrix_diagonal<__item__, ROW, COL>& b) :
+                ele {new __item__[diagonal]} {
+                std::copy(b.ele, b.ele+diagonal, ele);
+        };
+
+        matrix_diagonal(matrix_diagonal<__item__, ROW, COL>&& b) noexcept :
+                ele {b.ele} {
+                b.ele  = nullptr;
+        };
+
+        ~matrix_diagonal() {delete [] ele;};
+
+        constexpr std::size_t row() const {return ROW;};
+        constexpr std::size_t col() const {return COL;};
+
+        __item__ operator()(std::size_t row, std::size_t col) const {
+                if (row >= ROW || col >= COL) {
+                        throw std::out_of_range("Matrix subscribe out of range!\n");
+                }
+                if (row == col) {
+                        return ele[row];
+                } else {
+                        return 0;
+                }
+        };
+
+        matrix_diagonal<__item__, ROW, COL> operator+ () const {
+                matrix_diagonal<__item__, ROW, COL> c;
+                for (std::size_t i=0; i<diagonal; i++) {
+                       c.ele[i] = +(ele[i]);
+                }
+                return c;
+        };  //!< unary +
+
+        matrix_diagonal<__item__, ROW, COL> operator+ (const __item__ val) {
+                matrix_diagonal<__item__, ROW, COL> c;
+                for (std::size_t i=0; i<diagonal; i++) {
+                        c.ele[i] = ele[i] + val;
+                }
+                return c;
+        }  //!< !e implementation
+
+        matrix_diagonal<__item__, ROW, COL> operator- () const {
+                matrix_diagonal<__item__, ROW, COL> c;
+                for (std::size_t i=0; i<diagonal; i++) {
+                        c.ele[i] = -(ele[i]);
+                }
+                return c;
+        };  //!< unary -
+
+        matrix_diagonal<__item__, ROW, COL> operator- (const __item__ val) {
+                matrix_diagonal<__item__, ROW, COL> c;
+                for (std::size_t i=0; i<diagonal; i++) {
+                        c.ele[i] = ele[i] - val;
+                }
+                return c;
+        }  //!< !e implementation
+
+        matrix_diagonal<__item__, ROW, COL> operator* (const __item__ val) {
+                matrix_diagonal<__item__, ROW, COL> c;
+                for (std::size_t i=0; i<diagonal; i++) {
+                        c.ele[i] = ele[i] * val;
+                }
+                return c;
+        }
+
+        matrix_diagonal<__item__, ROW, COL>& operator= (const matrix_diagonal<__item__, ROW, COL>& b) {
+                std::copy(b.ele, b.ele+diagonal, ele);
+                return *this;
+        }
+
+        matrix_diagonal<__item__, COL, ROW> transpose() const {
+                matrix_diagonal<__item__, COL, ROW> c;
+                std::copy(ele, ele+diagonal, c.ele);
+                return c;
+        }
+
+        std::ostream& output (std::ostream& out) const {
+                for (std::size_t i=0; i<ROW; i++) {
+                        for (std::size_t j=0; j<COL; j++) {
+                                if (i >= diagonal || j >= diagonal) {
+                                        out << 0 << "  ";
+                                } else if (i == j) {
+                                        out << ele[i] << "  ";
+                                } else {
+                                        out << 0 << "  ";
+                                }
+                        }
+                        out << std::endl;
+                }
+                return out;
+        }
+};
+
+template <class __item__, std::size_t ROW, std::size_t COL>
+std::ostream& operator<< (std::ostream& out, matrix_diagonal<__item__, ROW, COL> b) {
+        return b.output(out);
+}
+
+template <class __item__, std::size_t ROW, std::size_t COL>
+matrix_diagonal<__item__, ROW, COL> operator+ (
+                const matrix_diagonal<__item__, ROW, COL>& a,
+                const matrix_diagonal<__item__, ROW, COL>& b) {
+        matrix_diagonal<__item__, ROW, COL> c;
+        for (std::size_t i=0; i<matrix_diagonal<__item__, ROW, COL>::diagonal; i++) {
+                c.ele[i] = a.ele[i] + b.ele[i];
+        }
+        return c;
+};
+
+template <class __item__, std::size_t ROW, std::size_t COL>
+matrix_diagonal<__item__, ROW, COL> operator- (
+                const matrix_diagonal<__item__, ROW, COL>& a,
+                const matrix_diagonal<__item__, ROW, COL>& b) {
+        matrix_diagonal<__item__, ROW, COL> c;
+        for (std::size_t i=0; i<matrix_diagonal<__item__, ROW, COL>::diagonal; i++) {
+                c.ele[i] = a.ele[i] - b.ele[i];
+        }
+        return c;
+};
+
+template <class __item__, std::size_t ROW, std::size_t N, std::size_t COL>
+matrix_diagonal<__item__, ROW, COL> operator* (
+                const matrix_diagonal<__item__, ROW, N>& a,
+                const matrix_diagonal<__item__, N, COL>& b) {
+        __item__ temp = 0;
+        matrix_diagonal<__item__, ROW, COL> c;
+        for (std::size_t i=0; i<std::min(std::min(ROW, N), COL); i++) {
+                c.ele[i] = a.ele[i]*b.ele[i];
+        }
+        return c;
+};
 
 #endif  //!< _MATRIX_DIAGONAL_HH_
 
