5e74e80264a4103992ea1ccb2095eefb5b13e77d
Shylock-Hg <tcath2s@gmail.com>
2019-01-08 13:46:56 +0000

    Define the base matrix-represented maze.

---
+++ algorithm/chapter08-stack/maze.cc
@@ -0,0 +1,11 @@
+/*! \brief 
+ *  \author Shylock Hg
+ *  \date 2019-01-08
+ *  \email tcath2s@gmail.com
+ * */
+
+#include "maze.hh"
+
+int main(int argc, char * argv[]) {
+        return 0;
+}
--- algorithm/chapter08-stack/maze.hh
+++ algorithm/chapter08-stack/maze.hh
@@ -7,7 +7,30 @@
 #ifndef _MAZE_HH_
 #define _MAZE_HH_
 
-class maze {};
+#include "../chapter07-matrix/matrix_array.hh"
+
+#include <utility>
+#include <vector>
+
+/*! \brief the algorithm to get the path of maze
+    \note true for pass, false for block
+*/
+template <std::size_t ROW, std::size_t COL>
+class maze : matrix_array<bool, ROW, COL> {
+public:
+        /*! \brief find the path from start to end
+            \param start start position
+            \param end end position
+            \retval the path sequence, empty for no way
+        */
+        std::vector<std::pair<std::size_t, std::size_t>> path(
+                std::pair<std::size_t, std::size_t> start,
+                std::pair<std::size_t, std::size_t> end) const {
+                std::vector<std::pair<std::size_t, std::size_t>> path;
+                //!< \TODO to find the path
+                return path;  //!< automatic move
+        }
+};
 
 #endif  //!< _MAZE_HH_
 
